9335bbe279a11de1537a8dafd48411c78c309a96
programmer <omkarsk98@gmail.com>
2019-07-10 10:25:14 +0530

    Sherlock and Anagrams solved

--- HackerRank/InterviewPreparation/HashTables/SherlockAndAnagrams.js
+++ HackerRank/InterviewPreparation/HashTables/SherlockAndAnagrams.js
@@ -1,50 +1,22 @@
 console.time("Code");
 let s = "abba";
 
-let checkAnagrams = (s1, s2) => {
-  s1.split("");
-  s2.split("");
-  for (let i of s1) {
-    if (!s2.includes(i)) return false;
-    else {
-      s2.replace(i, "");
-    }
-  }
-  console.log("Anagrams for", s1, s2);
-  return true;
-};
-
-let getSubSets = s => {
-  var i,
-    j,
-    result = [];
-
-  for (i = 0; i < s.length; i++) {
-    for (j = i + 1; j < s.length + 1; j++) {
-      let temp = s.slice(i, j);
-      if (temp.length !== s.length) result.push(s.slice(i, j));
-    }
-  }
-  let final = [];
-  for (let i of result) {
-    final.push(
-      i
+function sherlockAndAnagrams(s) {
+  let count = 0;
+  for (let i = 1; i < s.length; i++) {
+    let found = {};
+    for (let j = 0; j + i <= s.length; j++) {
+      let substr = s.substr(j, i);
+      substr = substr
         .split("")
         .sort()
-        .join("")
-    );
-  }
-  console.log(final);
-  return final;
-};
-
-function sherlockAndAnagrams(s) {
-  let subSets = getSubSets(s);
-  let count = 0;
-  for (let i of subSets) {
-    for (let j of subSets) {
-      if (i.length !== j.length) continue;
-      if (checkAnagrams(i, j)) count++;
+        .join("");
+      if (found[substr]) {
+        count += found[substr];
+        found[substr]++;
+      } else {
+        found[substr] = 1;
+      }
     }
   }
   return count;
